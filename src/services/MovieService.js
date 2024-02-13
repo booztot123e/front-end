1ebce5904ff466582ccba5359ac4492e8942752c
booztot123e <tros2545@hotmail.com>
2024-02-13 15:40:11 +0700

    edit save1

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,9 +1,17 @@
+import { env } from "../env.js";
+import { apiConfig } from "../apiConfig.js";
+
 export async function searchMovie(search_data) {
   try {
     console.log("search_data: " + search_data);
-
     const response = await fetch(
-      "http://api.se-rmutl.net/api/movie/search?search_text=" + search_data
+      apiConfig[env].API_URL + "/api/movie/search?search_text=" + search_data,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${apiConfig[env].bearer_token}`,
+        },
+      }
     );
 
     return await response.json(); //***
@@ -16,11 +24,17 @@ export async function searchMovie(search_data) {
 
 export async function getAllMovies() {
   try {
-    //const response = await fetch('/api/users');
-    //const response = await fetch('/api/movie/all');
-    const response = await fetch("http:///api.se-rmutl.net/api/movie/all");
+    console.log("env myConfig: " + env);
+    console.log("apiConfig.bearer_token: " + apiConfig[env].bearer_token);
+    console.log("apiConfig.API_URL: " + apiConfig[env].API_URL);
 
-    //const response = await fetch('/api/movie/all');
+    //const response = await fetch('http://localhost:4000/api/movie/all',
+    const response = await fetch(apiConfig[env].API_URL + "/api/movie/all", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${apiConfig[env].bearer_token}`,
+      },
+    });
     return await response.json();
   } catch (error) {
     return [];
@@ -28,14 +42,21 @@ export async function getAllMovies() {
 }
 
 export async function createMovie(data) {
-  const response = await fetch(`http:///api.se-rmutl.net/api/movie/insert`, {
-    //mode: 'no-cors',
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data),
-  });
-  console.log("Create Movie response: " + (await response));
+  try {
+    const response = await fetch(apiConfig[env].API_URL + "/api/movie/insert", {
+      //mode: 'no-cors',
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiConfig[env].bearer_token}`,
+      },
+      body: JSON.stringify(data),
+    });
+    console.log("Create Movie response: " + (await response));
 
-  // return  response.json();
-  return response;
+    // return  response.json();
+    return response;
+  } catch (error) {
+    return [];
+  }
 }
