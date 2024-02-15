419509701275b6105501a924d773c8c57967e493
booztot123e <tros2545@hotmail.com>
2024-02-15 14:04:17 +0700

    edit apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 export const apiConfig = {
   development: {
     bearer_token: "1234567890",
-    API_URL: "http://localhost:4000",
+    API_URL: "https://api.se-rmutl.net",
   },
   production: {
     bearer_token: "1234567890",
